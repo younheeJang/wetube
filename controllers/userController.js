6fcfa084e8a3c9fae392b579fc5d52e781652cab
curious jeager <edam7635@gmail.com>
2019-04-26 00:21:13 +0900

    passport.js used

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,11 @@
+import passport from 'passport';
 import routes  from '../routes';
+import User from '../models/User';
 
 export const getJoin = (req, res)=>{
     res.render('join',{pageTitle:'Join'})
 };
-export const postJoin = (req, res)=>{
+export const postJoin = async(req, res, next)=>{
     const {
         body:{name, email, password, password2}
     }=req;
@@ -11,14 +13,25 @@ export const postJoin = (req, res)=>{
         res.status(400);
         res.render('join',{pageTitle:'Join'});
     }else{
-        res.redirect(routes.home);
+        try{
+            const user = await User({
+                name, email
+            });
+            await User.register(user, password);
+            next();
+        }catch(error){
+            console.log(error);
+            res.redirect(routes.home);
+
+        }
     }
 };
 
 export const getLogin = (req, res)=>res.render('login',{pageTitle:'Login'});
-export const postLogin = (req, res)=>{
-    res.redirect(routes.home);
-}
+export const postLogin = passport.authenticate('local', {
+    failureRedirect:routes.login,
+    successRedirect:routes.home
+});
 
 export const logout = (req, res)=>{
     res.redirect(routes.home);
--- init.js
+++ init.js
@@ -1,10 +1,12 @@
+import dotenv from 'dotenv';
 import './db';
 import app from './app';
-import dotenv from 'dotenv';
+
 dotenv.config();
 import js from './js';
 import './models/Video';
 import './models/Comment';
+import './models/User';
 
 const PORT = process.env.PORT||4000;
 
