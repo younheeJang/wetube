6464e3fcdd4cac1b92d12e24dd99292d5a7a43f1
curious jeager <edam7635@gmail.com>
2019-04-27 01:56:55 +0900

    session

--- middlewares.js
+++ middlewares.js
@@ -2,10 +2,12 @@ import multer from 'multer';
 import routes from "./routes";
 
 const multerVideo = multer({dest:'uploads/videos/'});
+
 export const localsMiddleware=(req,res,next)=>{
     res.locals.siteName ='WeTube';
     res.locals.routes = routes;
-    res.locals.user = req.user||{};
+    res.locals.user = req.user||null;
+    console.log(req.user);
     next();
 }
 
