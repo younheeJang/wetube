4e12af23fc42aa3a031beecc1af46fb1127dbdcd
curious jeager <edam7635@gmail.com>
2019-05-05 02:56:38 +0900

    during recording

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,42 @@
+const recorderContainer = document.getElementById('#jsRecordContainer');
+const recordBtn = document.getElementById('jsRecordBtn');
+const videoPreview = document.getElementById('jsVideoPreview');
+
+let streamObject;
+
+const handleVideoData = (event) => {
+    
+}
+
+const startRecording = () => {
+    const videoRecorder = new MediaRecorder(streamObject);
+    videoRecorder.start();
+    videoRecorder.addEventListener('dataavailable', handleVideoData);
+}
+
+const getVideo = async() => {
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia({
+            audio:true,
+            video:{width:1280, height:720}
+        });  
+        videoPreview.srcObject = stream;
+        videoPreview.muted = true;
+        videoPreview.play();
+        recordBtn.innerHTML = 'Stop recording';
+        streamObject = stream;
+        startRecording(stream);
+    }catch(error){
+        recordBtn.innerHTML = 'Cant record';
+    }finally{
+        recordBtn.removeEventListener('click', getVideo);
+    }
+}
+
+function init(){
+    recordBtn.addEventListener("click", getVideo);
+}
+
+if(recorderContainer){
+    init();
+}
